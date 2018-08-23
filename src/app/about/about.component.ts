6dcc1caa5c19da4eb76087cb894624e6cead6663
ErkHal <erkkihalinen@gmail.com>
2018-08-23 16:26:49 +0300

    Added skills

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -14,6 +14,7 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
     ])
   ]
 })
+
 export class AboutComponent implements OnInit {
 
   constructor(private gitService: GithubService) { }
